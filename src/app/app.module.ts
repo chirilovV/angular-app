7373ca66e53a26b82558a3150d224a7a2d3e78a6
VeronicaChirilov <VeronicaChirilov@coherentslutions.com>
2022-08-18 13:44:51 +0300

    structure improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {WelcomeComponent} from './components/welcome/welcome.component';
 import {HomePageComponent} from './pages/home/home-page.component';
 import {NewsPageComponent} from './pages/news/news-page.component';
 import {PageNotFoundComponent} from './pages/not-found-404/page-not-found.component';
+import {AppRoutingModule} from "./modules/routingModule";
+import {AboutBannerComponent} from "./pages/home/about-banner/about-banner.component";
+import {ArticleComponent} from "./pages/news/article/article.component";
+import {BrowserModule} from "@angular/platform-browser";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
-import {AppRoutingModule} from "./modules/routingModule";
-import { AboutBannerComponent } from './pages/home/about-banner/about-banner.component';
-import { ArticleComponent } from './pages/news/article/article.component';
-import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
+
 
 @NgModule({
   declarations: [
@@ -29,7 +29,6 @@ import {MatCardModule} from "@angular/material/card";
     AppRoutingModule,
     MatButtonModule,
     MatToolbarModule,
-    MatGridListModule,
     MatCardModule,
   ],
   providers: [],
--- src/app/pages/news/article/article.component.spec.ts
+++ src/app/pages/news/article/article.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WelcomeComponent } from './welcome.component';
+import { ArticleComponent } from './article.component';
 
-describe('WelcomeComponent', () => {
-  let component: WelcomeComponent;
-  let fixture: ComponentFixture<WelcomeComponent>;
+describe('ArticleComponent', () => {
+  let component: ArticleComponent;
+  let fixture: ComponentFixture<ArticleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ]
+      declarations: [ ArticleComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(WelcomeComponent);
+    fixture = TestBed.createComponent(ArticleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
