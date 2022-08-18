9c5e2c01c11b32e4c3051e3e6e631ae24c9a45c4
VeronicaChirilov <VeronicaChirilov@coherentslutions.com>
2022-08-18 13:45:07 +0300

    structure improvement

---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  public pageTitle: string | undefined;
+
+  @Input()
+  title: string | undefined;
+
+  ngOnInit(): void {
+    this.pageTitle = this.title
+  }
+
+}
