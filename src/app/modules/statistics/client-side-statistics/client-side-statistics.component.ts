d17c080304dcad64f2a737393d8caa12fd66bf4e
VeronicaChirilov <VeronicaChirilov@coherentslutions.com>
2022-10-12 14:13:56 +0300

    refactored tables

--- src/app/modules/statistics/client-side-statistics/client-side-statistics.component.ts
+++ src/app/modules/statistics/client-side-statistics/client-side-statistics.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
+import {MatSort, MatSortable} from '@angular/material/sort';
 import {LocalUsersService} from '../../users/services/local-users-service';
 import {MatTableDataSource} from '@angular/material/table';
 import {UserStatisticsColumnsName} from '../enums/userStatisticsColumnsName';
-
+import {LocalUser} from '../../users/models/new-user.interface';
 
 @Component({
   selector: 'app-client-side-statistics',
   templateUrl: './client-side-statistics.component.html',
   styleUrls: ['./client-side-statistics.component.scss']
 })
-export class ClientSideStatisticsComponent implements OnInit {
+export class ClientSideStatisticsComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   columnsName = UserStatisticsColumnsName;
+  dataSource: MatTableDataSource<LocalUser> = new MatTableDataSource();
+
   displayedColumns: UserStatisticsColumnsName[] = [
     UserStatisticsColumnsName.name,
     UserStatisticsColumnsName.email,
@@ -21,34 +25,43 @@ export class ClientSideStatisticsComponent implements OnInit {
     UserStatisticsColumnsName.department,
     UserStatisticsColumnsName.addresses
   ];
-  dataSource: any = [];
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+
 
   constructor(private usersApiService: LocalUsersService) { }
 
   ngOnInit(): void {
     this.usersApiService.getUsers().subscribe(response => {
       this.dataSource = new MatTableDataSource(response);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-      console.log(this.dataSource);
-
-      this.dataSource.sortingDataAccessor = (item: any, property: any) => {
-        switch(property){
-          case 'name':
-            return item.personalInfo.lastName;
-          case 'email':
-            return item.personalInfo.email;
-          case 'age':
-            return item.personalInfo.age;
-          case 'department':
-            return item.companyInfo.department;
-          default:
-            return item[property];
-        }
-      };
+      console.log(response);
+//      this.initTableConfig();
     });
   }
+
+  public ngAfterViewInit(): void {
+    this.initTableConfig();
+  }
+
+  private initTableConfig(): void {
+    this.sort.sort(({id: UserStatisticsColumnsName.name, start: 'asc', disableClear: false}) as MatSortable);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = this.sortingDataAccessor;
+  }
+
+  private sortingDataAccessor = (item: LocalUser, property: string) => {
+    switch(property){
+      case 'name':
+        return item.personalInfo.lastName;
+      case 'email':
+        return item.personalInfo.email;
+      case 'age':
+        return item.personalInfo.age;
+      case 'department':
+        return item.companyInfo.department;
+      default:
+        //@ts-ignore
+        return item[property];
+    }
+  };
 
 }
--- src/app/modules/statistics/server-side-statistics/server-side-statistics.component.ts
+++ src/app/modules/statistics/server-side-statistics/server-side-statistics.component.ts
@@ -1,70 +1,65 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatSort} from '@angular/material/sort';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
-import {UsersApiService} from '../../users/services/users-api.service';
-import {PageOptions} from '../../shared/models/pageOptions';
 import {MatPaginator} from '@angular/material/paginator';
+import {UserStatisticsColumnsName} from '../enums/userStatisticsColumnsName';
+import {LocalUser} from '../../users/models/new-user.interface';
+import {LocalUsersService} from '../../users/services/local-users-service';
+import {MatSort, MatSortable} from '@angular/material/sort';
 
 @Component({
   selector: 'app-server-side-statistics',
   templateUrl: './server-side-statistics.component.html',
   styleUrls: ['./server-side-statistics.component.scss']
 })
-export class ServerSideStatisticsComponent implements OnInit {
+export class ServerSideStatisticsComponent implements OnInit, AfterViewInit {
+  displayedColumns: UserStatisticsColumnsName[] = [
+    UserStatisticsColumnsName.name,
+    UserStatisticsColumnsName.email,
+    UserStatisticsColumnsName.age,
+    UserStatisticsColumnsName.department,
+    UserStatisticsColumnsName.addresses
+  ];
 
-  displayedColumns: string[] = ['login', 'id', 'url'];
-  dataSource: any = [];
-  isLoading: boolean = false;
-  users: any[] = [];
+  dataSource: MatTableDataSource<LocalUser> = new MatTableDataSource<LocalUser>([]);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private usersService: UsersApiService) { }
+  constructor(private usersApiService: LocalUsersService) { }
 
-  ngOnInit(): void {
-    this.getAllUsers();
+
+  public ngOnInit(): void {
+    this.usersApiService.getUsers().subscribe(response => {
+      this.dataSource = new MatTableDataSource(response);
+      this.initTableConfig();
+      console.log(response);
+    });
   }
 
-  getAllUsers(pageOptions?: PageOptions): void {
-    console.log(pageOptions);
-    this.isLoading = true;
-    this.usersService.getUsers(pageOptions)
-    .subscribe(response => {
-        this.isLoading = false;
-        this.users = response.items;
-        this.users.length = response.total_count;
-
-        this.dataSource = new MatTableDataSource(this.users);
-        this.dataSource.paginator = this.paginator;
-      },
-    );
+  ngAfterViewInit(): void {
+//    this.initTableConfig();
   }
 
-  getNextData(currentSize: number, pageOptions: PageOptions) {
-    this.usersService.getUsers(pageOptions)
-    .subscribe(response => {
-        this.isLoading = false;
-        this.users.length = currentSize;
-        this.users.push(...response.items);
-        this.users.length = response.total_count;
-
-        this.dataSource = new MatTableDataSource(this.users);
-        this.dataSource._updateChangeSubscription();
-        this.dataSource.paginator = this.paginator;
-      },
-    );
+  private initTableConfig(): void {
+    this.sort.sort(({id: UserStatisticsColumnsName.name, start: 'asc'}) as MatSortable);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.dataSource.sortingDataAccessor = this.sortingDataAccessor;
   }
 
-  pageChanged(event: any): void {
-    this.isLoading = true;
-    let previousSize = event.pageIndex * event.pageSize;
-
-    let pageOptions: PageOptions = {
-      pageIndex: event.pageIndex,
-      pageSize: event.pageSize
-    };
-
-    this.getNextData(previousSize, pageOptions);
-  }
+  private sortingDataAccessor = (item: LocalUser, property: string) => {
+    switch(property){
+      case 'name':
+        return item.personalInfo.lastName;
+      case 'email':
+        return item.personalInfo.email;
+      case 'age':
+        return item.personalInfo.age;
+      case 'department':
+        return item.companyInfo.department;
+      default:
+        //@ts-ignore
+        return item[property];
+    }
+  };
 }
